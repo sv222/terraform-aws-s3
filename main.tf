1dcf713664dcdfdfd1e772b48d9fb2abfe0ea5d5
Stanislav <sv_golang@mail.ru>
2023-02-22 22:27:38 +0100

    implement recources

---
+++ main.tf
@@ -0,0 +1,58 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+  acl    = "private"
+  region = var.region
+
+  versioning {
+    enabled = var.versioning
+  }
+
+  lifecycle_rule {
+    id     = "default-rule"
+    status = "Enabled"
+    prefix = ""
+    tags   = {}
+
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    expiration {
+      days = 90
+    }
+  }
+
+  dynamic "lifecycle_rule" {
+    for_each = var.lifecycle_rule
+
+    content {
+      id     = lifecycle_rule.value["id"]
+      status = lifecycle_rule.value["status"]
+      prefix = lifecycle_rule.value["prefix"]
+      tags   = lifecycle_rule.value["tags"]
+
+      dynamic "transition" {
+        for_each = lifecycle_rule.value["transition"]
+
+        content {
+          days          = transition.value["days"]
+          storage_class = transition.value["storage_class"]
+        }
+      }
+
+      dynamic "expiration" {
+        for_each = lifecycle_rule.value["expiration"]
+
+        content {
+          days = expiration.value["days"]
+        }
+      }
+    }
+  }
+}
+
